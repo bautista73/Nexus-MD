fec2f21b4e9e2df3af6d9ea7f383399d0202db82
alex-rrr999 <125165948+alex-rrr999@users.noreply.github.com>
2023-04-07 15:18:16 -0400

    calendar js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
+
+const now = new Date();
+  
+const daysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
+const daysInPrevMonth = new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+const daysInNextMonth = new Date(now.getFullYear(), now.getMonth() + 2, 0).getDate();
+
+const firstDayOfWeek = new Date(now.getFullYear(), now.getMonth(), 1).getDay();
+
+const table = document.querySelector('.table-calendar');
+
+
+
+
 function updateDateTime() {
   var today = new Date();
 
@@ -27,26 +41,11 @@ function updateDateTime() {
 }
 
 
-
-
-
 function generateCalendar() {
-  // Get current date
-  const now = new Date();
+
   
-  // Get number of days in current month
-  const daysInMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate();
-  
-  // Get day of the week the first day falls on (0 = Sunday, 1 = Monday, etc.)
-  const firstDayOfWeek = new Date(now.getFullYear(), now.getMonth(), 1).getDay();
-  
-  // Get table element with class "table-calendar"
-  const table = document.querySelector('.table-calendar');
-  
-  // Clear table
   table.innerHTML = '';
   
-  // Create table header row
   const headerRow = document.createElement('tr');
   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   for (let i = 0; i < 7; i++) {
@@ -56,23 +55,62 @@ function generateCalendar() {
   }
   table.appendChild(headerRow);
   
-  // Create table rows for each week
-  let dayOfMonth = 1;
-  for (let i = 0; i < 6; i++) {
-    const tr = document.createElement('tr');
-    for (let j = 0; j < 7; j++) {
-      const td = document.createElement('td');
-      if (i === 0 && j < firstDayOfWeek) {
-        // Empty cell before first day of month
-      } else if (dayOfMonth <= daysInMonth) {
-        td.textContent = dayOfMonth;
-        dayOfMonth++;
-      }
-      tr.appendChild(td);
+let dayOfMonth = 1;
+let nextMonthDay = 1;
+
+for (let i = 0; i < 6; i++) {
+  const tr = document.createElement('tr');
+
+  for (let j = 0; j < 7; j++) {
+
+    const td = document.createElement('td');
+
+    if (i === 0 && j < firstDayOfWeek) {
+      td.textContent = daysInPrevMonth - (firstDayOfWeek - j - 1);
+    } else if (dayOfMonth <= daysInMonth) {
+      td.textContent = dayOfMonth;
+      dayOfMonth++;
+    } else {
+      td.textContent = nextMonthDay;
+      nextMonthDay++;
     }
-    table.appendChild(tr);
+
+    tr.appendChild(td);
   }
+  table.appendChild(tr);
 }
+}
+
+
+function nextMonth() {
+  const now = new Date();
+  now.setMonth(now.getMonth() + 1);
+
+  generateCalendar();
+
+  console.log('next')
+
+}
+
+function prevMonth() {
+  const now = new Date();
+  now.setMonth(now.getMonth() - 1);
+
+  generateCalendar();
+
+  console.log('prev')
+
+}
+
+
+
+
+
+document.getElementById("nextMonthButton").addEventListener("click", nextMonth);
+document.getElementById("prevMonthButton").addEventListener("click", prevMonth);
+
+
+
 
 
 generateCalendar();
@@ -90,38 +128,3 @@ setInterval(updateDateTime, 1000);
 
 
 
-
-// function updateDateTime() {
-//     var today = new Date();
-  
-//     var options = { month: 'long', day: 'numeric', year: 'numeric' };
-//     var optionscal = { month: 'long', year: 'numeric' };
-
-//     var date = today.toLocaleDateString('en-US', options).replace(/(\d)(st|nd|rd|th)/, '$1<sup>$2</sup>');
-//     var datecal = today.toLocaleDateString('en-US', optionscal).replace(/(\d)(st|nd|rd|th)/, '$1<sup>$2</sup>');
-  
-
-
-//     var hours = today.getHours();
-//     var minutes = today.getMinutes();
-//     var amOrPm = hours >= 12 ? 'PM' : 'AM';
-
-//     hours = hours % 12;
-//     hours = hours ? hours : 12; 
-//     minutes = minutes < 10 ? '0'+minutes : minutes;
-    
-//     var time = hours + ':' + minutes + ' ' + amOrPm;
-  
-//     var dateTime = date+' | '+time;
-
-//     var dateTimeCal = datecal
-  
-//     document.getElementById("datetime").innerHTML = dateTime;
-
-//     document.getElementById("datetime-cal").innerHTML = dateTimeCal;
-
-//   }
-  
-//   updateDateTime();
-  
-//   setInterval(updateDateTime, 1000); 
